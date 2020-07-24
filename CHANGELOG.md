54dce12151e43338c722f25f45e606a51289e799
workcom <25128289+LittleHeronCodes@users.noreply.github.com>
2020-07-24 19:58:05 +0900

    enrichment Dataframe minor fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Changelog
 
+## Ver 0.0.3.2 [2020.07.23]
+
+* `enrichmentFactorForDataFrame`
+  * returns dataframe
+  * calculates actual coverage
+  * Added results : logP, FDR, logQ
+  * fixed potentially critical error with hgeo ordering
+
 ## Ver 0.0.3.1 [2020.07.16]
 
 * patch 1 : Fix ent2sym (geneMap to LazygeneInfo)
--- R/CMAP_style_gsea.R
+++ R/CMAP_style_gsea.R
@@ -7,12 +7,11 @@
 #' @param cutoff cut percentile. default 0.05
 #' @param min_count minimum count to be valid. default 3
 #' @param psc pseudocount when caclulating enrichment. default 1
-#' @return list of named vectors for each category
+#' @return dataframe of named vectors for each category
 #' @export
 
 ## Calculate enrichment factor for drugs
-enrichmentFactorForDataFrame = function(df, enrich_element, rank_by, 
-	cutoff = 0.05, min_count = 3, psc=1) {
+enrichmentFactorForDataFrame = function(df, enrich_element, rank_by, cutoff = 0.05, min_count = 3, psc=1) {
 
 	# index to use
 	include = names(which(table(df[, enrich_element]) >= min_count))
@@ -23,6 +22,9 @@ enrichmentFactorForDataFrame = function(df, enrich_element, rank_by,
 
 	# order df by rank, get only within cut off
 	rowIdx = which(rank(-df[,rank_by], ties.method = 'max') < nrow(df)*cutoff)
+
+	# actual coverage
+	cover = length(rowIdx) / nrow(df)
 
 	# counts
 	cntAll = table(df[, enrich_element])		# total for each
@@ -37,7 +39,7 @@ enrichmentFactorForDataFrame = function(df, enrich_element, rank_by,
 
 	# hypergeo test
 	hp = phyper(cntPct[idx]-1, cntAll[idx], N-cntAll[idx], k, lower.tail = FALSE)
-	hp = sort(hp)
+	hp = hp[names(ef)]
 	# lhp = -log10(hp)
 
 	# ratio
@@ -52,7 +54,15 @@ enrichmentFactorForDataFrame = function(df, enrich_element, rank_by,
 	# lhp2 = structure(rep(0, length(idx2)), names=idx2)
 	rat2 = structure(paste0(0, '/', cntAll[idx2]), names=idx2)
 
-	out = list(EF= c(ef, ef2), HyperP=c(hp, hp2), ratio=c(rat, rat2))
+	out = list(EF= c(ef, ef2), HyperP=c(hp, hp2), ratio=c(rat, rat2), cover = cover)
+	out = data.frame(out)
+
+	## More info
+	out$logP = -log10(out$HyperP)
+	out$qVal = p.adjust(out$HyperP, method='fdr')
+	out$logQ = -log10(out$qVal)
+	
+	out = out[,c('EF','HyperP','logP','qVal','logQ','ratio','cover')]
 	return(out)
 }
 
