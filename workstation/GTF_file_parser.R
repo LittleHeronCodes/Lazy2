006a26fc12489d9bc7b4e04cfa292b10d0ddcf34
workcom <25128289+LittleHeronCodes@users.noreply.github.com>
2020-11-20 18:19:29 +0900

    additional functions, gtf file parser for mouse gene info

---
+++ workstation/GTF_file_parser.R
@@ -0,0 +1,23 @@
+## GTF File parse
+
+library(data.table)
+library(rtracklayer)
+
+gtf_path <- "gencode.vM24.primary_assembly.annotation.gtf"
+
+# GTF <- fread(gtf_path)
+gtf <- readGFF(gtf_path)
+
+# gtfDT <- as.data.table(gtf)
+
+genes <- gtf[which(gtf$type == 'gene'),c('gene_id','seqid','gene_type','gene_name','mgi_id')]
+table(genes$gene_type)
+
+colnames(genes)[3] <- 'gene_type_raw'
+
+genes$gene_type <- genes$gene_type_raw
+genes$gene_type[grep('protein_coding', genes$gene_type_raw)] <- 'protein-coding gene'
+genes$gene_type[grep('pseudogene$'   , genes$gene_type_raw)] <- 'pseudogene'
+
+fwrite(genes, file='data/others/mousegenes_from_gtf.csv')
+
